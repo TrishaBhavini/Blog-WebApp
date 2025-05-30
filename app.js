155462edccdbf89dc4013e49fbafbaa9fd86e3c8
Trisha Bhavini <90496506+TrishaBhavini@users.noreply.github.com>
2025-05-30 13:19:01 +0530

    Update app.js

--- app.js
+++ app.js
@@ -49,7 +49,7 @@ app.post("/delete/:id", (req, res) => {
 });
 
 // Server
-const port = 3000;
+const port = process.env.PORT || 3000;
 app.listen(port, () => {
   console.log(`Server started on port ${port}`);
 });
